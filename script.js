5d064fb4f5fb694c5610b8b13ec5b22b45e02d90
lucileide-Elpidio <luhelpidio23@outlook.com>
2023-07-08 11:11:37 -0300

    adicionei if else na função generateNumber

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function generateNumber() {
   const inputMax = Math.ceil(document.querySelector(".input-min").value)
   const inputMin = Math.floor(document.querySelector(".input-max").value)
   const drawNumber = document.querySelector(".drawnumber")
+  const msg = document.querySelector(".mensage")
 
-  const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
-
-  drawNumber.innerHTML = result
-
+  if (inputMax < inputMin) {
+    const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
+    drawNumber.innerHTML = result
+    msg.innerHTML = "O número sorteado é"
+  } else {
+    msg.innerHTML = "O primeiro número deve ser maior que o segundo número"
+  }
 }
 
 buttonSortear.addEventListener("click", generateNumber)
